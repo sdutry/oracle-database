b2b391a259e773c55dc5fe848b53b60a95609c62
Stefaan Dutry <stefaan.dutry@gmail.com>
2018-03-08 14:25:54 +0100

    add scripts to check dependencies

---
+++ dependencies_used_by.sql
@@ -0,0 +1,8 @@
+SELECT TYPE as type, 
+       OWNER || '.' || NAME as name,
+       REFERENCED_TYPE as referenced_type,
+       REFERENCED_OWNER || '.' || REFERENCED_NAME as referenced_name
+  FROM all_dependencies
+ WHERE     REFERENCED_NAME = UPPER(LTRIM(RTRIM( :object_name )))
+       AND REFERENCED_OWNER NOT IN ('SYS', 'SYSTEM', 'PUBLIC')
+       AND OWNER NOT IN ('SYS', 'SYSTEM', 'PUBLIC')
